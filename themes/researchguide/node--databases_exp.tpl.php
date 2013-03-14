b0c130f50a3c9efe78e22f07b79061fdc13e6479
Emily <emily@emily-workstation.(none)>
2013-03-14 11:27:52 -0400

    Added links to the node files.
    Now all point to node--database_list.tpl.php

--- themes/researchguide/node--databases_exp.tpl.php
+++ themes/researchguide/node--databases_exp.tpl.php
@@ -1,20 +1,25 @@
 <?php
 // $Id: node.tpl.php,v 1.7 2007/08/07 08:39:36 goba Exp $
-?>
-  <div class="node<?php if ($sticky) { print " sticky"; } ?><?php if (!$status) { print " node-unpublished"; } ?>">
-
-
-<?php
 
   global $language;
   global $base_url;
+
+  $node_has_content = false;
+
+  $node_class = 'node';
+  if ($sticky) {
+     $node_class .= ' sticky';
+  }
+  if (!$status) {
+     $node_class .= ' node-unpublished';
+  }
+  $node_out = "<div class='node $node_class'>";
 
   $page_type = 'a-z';
   $title_text = 'Databases A-Z';
   if ($language->language == 'fr') {
     $title_text = 'Bases de données, A-Z';
   }
-  $result = $field_databases;
 
   // Check for other kinds of database lists
   // This assumes that we don't wind up with a URL like "free-new-multidisciplinary"
@@ -41,7 +46,7 @@
   }
   
   if ($page_type != 'guides') {
-    print '<h1>' . $title_text . '</h1>';
+    $node_out .= '<h1>' . render($title_text) . '</h1>';
   }
 
   /*  We want all nodes that are:
@@ -49,8 +54,6 @@
    *   2. In the user's current language 
    *   3. Published
    */
-  
-  if ($page_type != 'guides') {
   $result = db_query("
     SELECT n.created, n.title, n.nid, n.changed
     FROM {node} n
@@ -60,7 +63,11 @@
       ORDER BY n.title ASC
     ", array(':language' => $language->language)
   );
-  } 
+
+  // Unless, of course, we're a specific database list
+  if ($page_type == 'guides') {
+    $result = $field_databases;
+  }
 
   $desc_header = 'Description: ';
   // Inline navigation via the wonder of anchors
@@ -79,9 +86,7 @@
     </p>
 HERE;
 
-?>
-<ul>
-<?php
+  $node_out .= "<ul>";
 
   // Place anchors roughly 1/5 of the way through the page
   $anchors = array('A', 'D', 'H', 'M', 'S', 'Z');
@@ -94,27 +99,30 @@ HERE;
       $nid = $i['nid'];
     }
     // Load the node contents for each database
-    $cur_db_node = node_load($i->nid);
-    if ($page_type == 'guides') {
-      $cur_db_node = node_load($nid);
-    }
+    $cur_db_node = node_load($nid);
+
     // Grab the URL & description
-    $cur_db_title = $i->title;
-    if ($page_type == 'guides') {
-      $cur_db_title = $cur_db_node->title;
-    }
+    $cur_db_title = $cur_db_node->title;
     $cur_db_url = $cur_db_node->field_database_url['und'][0]['url'];
+    // Most people won't enter ampersands properly in URLs, so fix it for them
+    if (!stristr($cur_db_url, '&amp;')) {
+        $cur_db_url = str_replace('&', '&amp;', $cur_db_url);
+    }
+
     $cur_db_desc = $cur_db_node->field_database_description['und'][0]['value'];
     $cur_db_multi = $cur_db_node->field_multidisciplinary['und'][0]['value'];
     $cur_db_proxy = $cur_db_node->field_proxied['und'][0]['value'];
     $cur_db_new = $cur_db_node->field_new['und'][0]['value'];
     $cur_db_expires = $cur_db_node->field_database_expiration_date['und'][0]['value2'];
 
+    // Skip the database unless it has a name & URL
+    if (!($cur_db_title && $cur_db_url)) {
+        continue;
+    }
     // Skip the database if it is expired
     if ($cur_db_expires && (strtotime($cur_db_expires) < strtotime(date('Y-m-d')))) {
        continue;
     }
-    if ($page_type != 'guides') {
     // Skip the database if it does not match a requested page type
     if ($page_type == 'multidis' && !$cur_db_multi) {
        continue;
@@ -122,7 +130,6 @@ HERE;
        continue;
     } elseif ($page_type == 'trials' && !$cur_db_expires) {
        continue;
-    } 
     }
 
     // Proxy 'em if you got 'em
@@ -135,39 +142,41 @@ HERE;
 
     if ($cur_db_desc) {
       $desc = '<p name="dbdesc_' . $nid . '" style="display:none;">';
-      $desc .= '<span width="25%"><b>' . $desc_header . '</b></span>';
+      $desc .= '<span style="width: 25%;"><b>' . $desc_header . '</b></span>';
       $desc .= '<span>' . $cur_db_desc . '</span>';
       $desc .= '</p>';
 
-      $db_display = "(<span onclick='jQuery(\"[name=\\\"dbdesc_$nid\\\"]\").toggle();' style='font-weight: bold'>?</span>)";
+      $db_display = "<span onclick='jQuery(\"[name=\\\"dbdesc_$nid\\\"]\").toggle();' style='font-weight: bold'>(?)</span>";
     }
 
     // Check current user's roles and show an edit button if admin or librarian
     if (in_array(librarian, $GLOBALS['user']->roles) || in_array(administrator, $GLOBALS['user']->roles)) {
        $db_edit = '&nbsp;<a class="db_edit_link" href="' . $base_url . '/node/' . $nid . '/edit">(Edit)</a>';
        $db_edit_list = '<p><a class="db_edit_link" href="' . $base_url . '/node/' . $node->nid . '/edit">Edit database list</a></p>';
+       // Print empty nodes for librarians
+       $node_has_content = true;
     }
-    if ($page_type != 'guides') {
+
     // Define an anchor, perhaps
-    if (substr($cur_db_title, 0, 1) == $init_ltr) {
+    if ($page_type == 'a-z' && substr($cur_db_title, 0, 1) == $init_ltr) {
       $list_id = " id='list_$init_ltr'";
       $init_ltr = array_shift($anchors);
-      print $jump_list;
+      $node_out .= $jump_list;
     }
-    }
-    if ($page_type != 'guides') {
-?>
-    <li<?php print $list_id; ?>><a href="<?php print $cur_db_url; ?>"><?php print render($cur_db_title); ?></a> <?php print render($db_display); ?><?php print render($db_edit); ?> </li>
-    <?php print render($desc); ?>
-<?php
-    }
-    elseif ($page_type == 'guides') {
-?>
-   <li><a href="<?php print $db_url; ?>"><?php print render($cur_db_title); ?></a><?php print render($db_display); ?><?php print render($db_edit); ?></li>
-       <?php print render($desc); ?>
-<?php
-    }
+
+    $li_entry = "<li$list_id><a href='$cur_db_url'>" . render($cur_db_title);
+    $li_entry .= "</a> " . render($db_display) . render($db_edit);
+    $li_entry .= render($desc) . "</li>";
+
+    $node_out .= $li_entry;
+
+    // If we've made it this far, we have content
+    $node_has_content = true;
   }
-  print ($db_edit_list);
+  $node_out .= $db_edit_list;
+  $node_out .= "</ul></div>";
+
+  if ($node_has_content) {
+    print($node_out);
+  }
 ?>
-</div>
