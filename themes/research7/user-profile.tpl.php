3ec9990149b2a1f69276716c4e35563b707aa87c
Emily Bamsey <em_bamsey@laurentian.ca>
2013-07-17 11:47:07 -0400

    Added French Translation to User Profile Link
    In the user-profile.tpl.php only the link to edit/
    view all the guides is available. This link now has
    the English and French version. Remaining translations
    are within another view (not a PHP one).
    
    Signed-off-by: Emily Bamsey <em_bamsey@laurentian.ca>

--- themes/research7/user-profile.tpl.php
+++ themes/research7/user-profile.tpl.php
@@ -7,8 +7,16 @@
 //Show owned content div only if a user is logged in
 global $user;
 global $base_url;
+global $language;
+
+$link = '';
 if ($user->uid != null && arg(0) == 'user') {
-?>
-  <h1><a href="<?php $base_url; ?>/research/admin-guides">
-    Search content within Research Guides</a></h1>
-<?php } ?>
+  if ($language->language == 'fr') {?>
+    <h1><a href="<?php $base_url; ?>/research/admin-guides">
+      Chercher dans les Guides de recherches</a></h1>
+  <?php } 
+  else {?>
+    <h1><a href="<?php $base_url; ?>/research/admin-guides">
+      Search content within Research Guides</a></h1>
+  <?php }
+}?>
